1a064bf7dba5d1ef18afff8e109d3ee10578ba3e
HH <275258836@qq.com>
2018-09-07 22:49:02 +0800

    添加 cobra

--- vendor/github.com/spf13/cobra/cobra/main.go
+++ vendor/github.com/spf13/cobra/cobra/main.go
@@ -1,10 +1,9 @@
-// Copyright © 2018 NAME HERE <EMAIL ADDRESS>
+// Copyright © 2015 Steve Francia <spf@spf13.com>.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
+// http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -14,7 +13,7 @@
 
 package main
 
-import "github.com/daiguadaidai/m-sql-review/cmd"
+import "github.com/spf13/cobra/cobra/cmd"
 
 func main() {
 	cmd.Execute()
