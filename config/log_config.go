8645b82522dd8bc5d1f74a167d6c59708ce48c1d
HH <275258836@qq.com>
2018-10-10 11:39:42 +0800

    修改日志出文件名

--- config/log_config.go
+++ config/log_config.go
@@ -4,7 +4,7 @@ func LogDefautConfig() string {
 	return `
         <seelog type="sync">
         	<outputs formatid="main">
-				<rollingfile type="size" filename="seelog.log" maxsize="1073741824" maxrolls="10" />
+				<rollingfile type="size" filename="sql_review.log" maxsize="1073741824" maxrolls="10" />
         	</outputs>
             <formats>
                 <format id="main" format="%Date %Time [%Level] %File:%Line %Msg%n"/>
